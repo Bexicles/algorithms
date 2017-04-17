370557583047b679c654c464292f72e7db143cf4
becci.denny <bexicles@gmail.com>
2017-04-17 19:55:43 +0100

    Now working in-place QuickSort algorithm

--- QuickSort/quicksort_pivot_median_inplace.py
+++ QuickSort/quicksort_pivot_median_inplace.py
@@ -1,9 +1,6 @@
 import statistics as stat
 
 
-count = 0
-
-
 def swap(A, a, b):
     temp = A[a]
     A[a] = A[b]
@@ -40,9 +37,6 @@ def partition(A, pivot, l, r, n):
     p = A.index(pivot)  # update pivot index to reflect new position
     i = p+1
 
-    global count
-    count += n - 1
-
     for j in range(p+1, n):
         if A[j] < pivot:
             A = swap(A, i, j)
@@ -54,33 +48,21 @@ def partition(A, pivot, l, r, n):
         len_1 = len(A[l:i-1])
         len_2 = len(A[i:r+1])
 
-        print("A is now", A, "and pivot is", pivot)
         if len_1 > 1:
-            print("Idx is", l, "to", i, "& new array is", A[l:i-1], "length", len_1)
-            quicksort(A, l, i-1, len_1)  # recurse on 'first half' of array, i.e. values less than pivot
+            A = quicksort(A, l, i-1, len_1)  # recurse on 'first half' of array, i.e. values less than pivot
 
         if len_2 > 1:
-            print("Idx is", i, "to", r, "& new array is", A[i:r+1], "length", len_2)
-            quicksort(A, i, r, len_2)  # recurse on 'second half' of array, i.e. values greater than pivot
+            A = quicksort(A, i, r, len_2)  # recurse on 'second half' of array, i.e. values greater than pivot
 
         return A
 
 
 def run_quicksort(A):
-    print("original array is", A)
-    global count
-    count = 0
 
     n = len(A)
     l = 0
     r = n-1
 
     A = quicksort(A, l, r, n)
-    print("sorted array is", A, "& there were", count, "comparisons")
 
-    return A, count
-
-
-
-B = [9, 11, 10, 3, 1, 16, 5, 4]
-run_quicksort(B)
+    return A
--- QuickSort/quicksort_tests.py
+++ QuickSort/quicksort_tests.py
@@ -1,25 +1,16 @@
 import unittest
-from quicksort_pivot_first import run_quicksort
-
-
-class SortArraysTest_2(unittest.TestCase):
-    def test_number_comparisons(self):
-        self.assertEqual(0, run_quicksort([5])[1])
-        self.assertEqual(1, run_quicksort([9, 7])[1])
-        self.assertEqual(4, run_quicksort([6, 7, 2, 1])[1])
-        self.assertEqual(3, run_quicksort([3, 2, 1])[1])
-        self.assertEqual(7, run_quicksort([3, 6, 11, 1, 8])[1])
-        self.assertEqual(9, run_quicksort([311, 206, 100, 184, 288])[1])
+from quicksort_pivot_median_inplace import run_quicksort
 
 
 class SortArraysTest_1(unittest.TestCase):
     def test_sorted_arrays(self):
 
-        self.assertEqual([5], run_quicksort([5])[0])
-        self.assertEqual([7,9], run_quicksort([9, 7])[0])
-        self.assertEqual([1,2,6,7], run_quicksort([6, 7, 2, 1])[0])
-        self.assertEqual([1,2,3], run_quicksort([3, 2, 1])[0])
-        self.assertEqual([1,3,6,8,11], run_quicksort([3, 6, 11, 1, 8])[0])
+        self.assertEqual([5], run_quicksort([5]))
+        self.assertEqual([7, 9], run_quicksort([9, 7]))
+        self.assertEqual([1, 2, 6, 7], run_quicksort([6, 7, 2, 1]))
+        self.assertEqual([1, 2, 3], run_quicksort([3, 2, 1]))
+        self.assertEqual([1, 3, 6, 8, 11], run_quicksort([3, 6, 11, 1, 8]))
+        self.assertEqual([1, 3, 5, 6, 8, 10, 11], run_quicksort([5, 3, 6, 11, 1, 8, 10]))
 
 
 
